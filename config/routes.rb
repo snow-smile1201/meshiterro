668bf2cab6d6484e58f9d83a057294c476bb3405
MasayukiHorimoto <141377771+snow-smile1201@users.noreply.github.com>
2023-09-12 03:13:07 +0000

    [add]4-18 削除機能ルーティングエラー修正途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :dest]
+  end
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: 'about'
